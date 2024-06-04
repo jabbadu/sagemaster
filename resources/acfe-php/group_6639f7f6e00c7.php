649f0c6966c9a4380c37f60aee28df4d714e87b0
Olaf Parusel <info@jabbadu.com>
2024-06-04 21:01:22 +0200

    sync acf

---
+++ resources/acfe-php/group_6639f7f6e00c7.php
@@ -0,0 +1,74 @@
+<?php 
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_6639f7f6e00c7',
+	'title' => 'Element: Text',
+	'fields' => array(
+		array(
+			'key' => 'field_6639f7f7e032e',
+			'label' => 'Text',
+			'name' => 'text',
+			'aria-label' => '',
+			'type' => 'wysiwyg',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'acfe_wysiwyg_auto_init' => 0,
+			'acfe_wysiwyg_height' => 300,
+			'acfe_wysiwyg_max_height' => '',
+			'acfe_wysiwyg_valid_elements' => '',
+			'acfe_wysiwyg_custom_style' => '',
+			'acfe_wysiwyg_disable_wp_style' => 0,
+			'acfe_wysiwyg_autoresize' => 0,
+			'acfe_wysiwyg_disable_resize' => 0,
+			'acfe_wysiwyg_remove_path' => 0,
+			'acfe_wysiwyg_menubar' => 0,
+			'acfe_wysiwyg_transparent' => 0,
+			'acfe_wysiwyg_merge_toolbar' => 0,
+			'acfe_wysiwyg_custom_toolbar' => 0,
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+			'delay' => 0,
+			'acfe_wysiwyg_min_height' => 300,
+			'acfe_wysiwyg_toolbar_buttons' => array(
+			),
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'post',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'left',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => false,
+	'description' => '',
+	'show_in_rest' => 0,
+	'acfe_autosync' => array(
+		0 => 'php',
+	),
+	'acfe_form' => 0,
+	'acfe_display_title' => '',
+	'acfe_meta' => '',
+	'acfe_note' => '',
+	'modified' => 1717527622,
+));
+
+endif;
---
+++ resources/acfe-php/group_6639fefef145d.php
@@ -0,0 +1,66 @@
+<?php 
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_6639fefef145d',
+	'title' => 'Eingangsbild',
+	'fields' => array(
+		array(
+			'key' => 'field_6639feff24327',
+			'label' => 'Eingangsbild',
+			'name' => 'featured_image',
+			'aria-label' => '',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'uploader' => '',
+			'return_format' => 'url',
+			'upload_folder' => '',
+			'acfe_thumbnail' => 1,
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'preview_size' => 'medium',
+			'library' => 'all',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'page',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'left',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+	'acfe_autosync' => array(
+		0 => 'php',
+	),
+	'acfe_form' => 0,
+	'acfe_display_title' => '',
+	'acfe_meta' => '',
+	'acfe_note' => '',
+	'modified' => 1717527628,
+));
+
+endif;
